28855e67c98d320b1f354f5f32eb50c9d75aac23
EvilCommunist <AT2415216@yandex.ru>
2023-03-09 12:03:41 +0300

    Замена
    Замена алгоритма определения структуры на менее громоздкий и более удобный

--- БМП/Photon2/Photon2.cpp
+++ БМП/Photon2/Photon2.cpp
@@ -20,77 +20,71 @@ int main()
         if (cnt < 1)
             file << "Сигнатура: ";
         pic.read(reinterpret_cast<char*>(&word), sizeof(word));
-        file << word;
+        file << (char)word;
         cnt += 1;
     }
     uint32_t bytesize;
     // Прочтение размера
-    pic.read(reinterpret_cast<char*>(&bytesize), sizeof(bytesize));
+    pic.read(reinterpret_cast<char*>(&bytesize), sizeof(uint8_t));
     file << endl << "Размер файла (байт): " << bytesize << endl;
     while (cnt < 6) // Проверка нулевых полей
     {
-        pic.read(reinterpret_cast<char*>(&word), sizeof(word));
+        pic.read(reinterpret_cast<char*>(&word), sizeof(uint8_t));
         file << "Зарезервированное поле " << cnt - 2 << " (проверка, длжно быть 0): " << word << endl;
         cnt += 1;
     }
     uint32_t changeplace;
     // Прочтение смещения
-    pic.read(reinterpret_cast<char*>(&changeplace), sizeof(changeplace));
+    pic.read(reinterpret_cast<char*>(&changeplace), sizeof(uint8_t));
     file << "Смещение (в байтах): " << changeplace << endl << endl;
     uint32_t struck;
     // Прочтение структуры
-    pic.read(reinterpret_cast<char*>(&struck), sizeof(struck));
+    pic.read(reinterpret_cast<char*>(&struck), sizeof(uint8_t));
     file << "Размер (в битах) структуры: " << struck << endl;
-    if (struck < 50)
+    // Определение типа структуры
+    switch (struck)
     {
-        if (struck < 25)
-            file << "Имя структуры: BITMAPCOREHEADER" << endl;
-        else
-            file << "Имя структуры: BITMAPINFOHEADER" << endl;
-    }
-    else
-    {
-        if (struck < 110)
-            file << "Имя структуры: BITMAPV4HEADER" << endl;
-        else
-            file << "Имя структуры: BITMAPV5HEADER" << endl;
+    case 12: file << "Имя структуры: BITMAPCOREHEADER" << endl; break;
+    case 40: file << "Имя структуры: BITMAPINFOHEADER" << endl; break;
+    case 108: file << "Имя структуры: BITMAPV4HEADER" << endl; break;
+    case 124: file << "Имя структуры: BITMAPV5HEADER" << endl; break;
     }
     long height, width;
     // Прочтение высоты
-    pic.read(reinterpret_cast<char*>(&height), sizeof(height));
+    pic.read(reinterpret_cast<char*>(&height), sizeof(uint8_t));
     file << "Высота (в пикселях): " << height << endl;
     // Прочтение ширины
-    pic.read(reinterpret_cast<char*>(&width), sizeof(width));
+    pic.read(reinterpret_cast<char*>(&width), sizeof(uint8_t));
     file << "Ширина (в пикселях): " << width << endl;
     // Прочтение "курсора"
-    pic.read(reinterpret_cast<char*>(&word), sizeof(word));
+    pic.read(reinterpret_cast<char*>(&word), sizeof(uint8_t));
     file << "Проверка курсора (должно быть 1): " << word << endl;
     uint16_t bpp;
     // Прочтение количества бит на пискель
-    pic.read(reinterpret_cast<char*>(&bpp), sizeof(bpp));
+    pic.read(reinterpret_cast<char*>(&bpp), sizeof(uint8_t));
     file << "Число бит на пиксель: " << bpp << endl;
     uint32_t savepic;
     // Прочтение способа хранения пикселей
-    pic.read(reinterpret_cast<char*>(&savepic), sizeof(savepic));
+    pic.read(reinterpret_cast<char*>(&savepic), sizeof(uint8_t));
     file << "Способ хранения пикселей: " << savepic << "   ///   BI_RGB (двумерный массив)" << endl; // В данном случае мы имеем способ BI_RGB (двумерный массив), примем это за факт.
     uint32_t picdata;
     // Прочтение размера пиксельных данных в байт
-    pic.read(reinterpret_cast<char*>(&picdata), sizeof(picdata));
+    pic.read(reinterpret_cast<char*>(&picdata), sizeof(uint8_t));
     file << "Размер пиксельных данных в байт: " << picdata << endl;
     long horpicpm, vertpicpm;
     // Прочтение кол-ва пикселей на метр по горизонтали
-    pic.read(reinterpret_cast<char*>(&horpicpm), sizeof(horpicpm));
+    pic.read(reinterpret_cast<char*>(&horpicpm), sizeof(uint8_t));
     file << "Кол-во пикселей на метр по горизонту: " << horpicpm << endl;
     // Прочтение кол-ва пикселей на метр по вертикали
-    pic.read(reinterpret_cast<char*>(&vertpicpm), sizeof(vertpicpm));
+    pic.read(reinterpret_cast<char*>(&vertpicpm), sizeof(uint8_t));
     file << "Кол-во пикскелей на метр по вертикали: " << vertpicpm << endl;
     uint32_t sizecolour;
     // Прочтение размера таблицы цветов
-    pic.read(reinterpret_cast<char*>(&sizecolour), sizeof(sizecolour));
+    pic.read(reinterpret_cast<char*>(&sizecolour), sizeof(uint8_t));
     file << "Размер таблицы цветов: " << sizecolour << endl;
     uint32_t lenghtcol;
     // Прочтение кол-ва ячеек от таблицы цветов до неё самой
-    pic.read(reinterpret_cast<char*>(&lenghtcol), sizeof(lenghtcol));
+    pic.read(reinterpret_cast<char*>(&lenghtcol), sizeof(uint8_t));
     file << "Кол-во ячеек от таблицы цветов до неё самой: " << lenghtcol << endl << "Заметим, что в данном случае цвета записаны в обратном порядке" << endl << endl << "Ниже представлена кодировка цветов:" << endl;
     //Можно сделать универсальное прочтение файла с помощью добавления условного счётчика типа "cond", но в данном случае принимаем "динамическое" прочтение BMP файла
     long size, h, w, i;
