e47c8db62d8097a84a9a781126faa908435c3bf7
EvilCommunist <AT2415216@yandex.ru>
2023-06-29 01:14:04 +0300

    Snap back to the roots
    Возвращение к предыдущему варианту выполнения задания, надо исправить вывод матрицы и хедера в файл

--- БМП/Photon2/Photon2.cpp
+++ БМП/Photon2/Photon2.cpp
@@ -10,7 +10,7 @@
 #include "Struct.h"
 #include "Pixel.h"
 #include "Matrix.h"
-#include "BMPdata.h"
+#include "Size.h"
 
 using byte = unsigned char;
 
@@ -50,60 +50,44 @@ T takeNextField(std::ifstream& stream) {
 
  //return смещение до начала пиксельных данных
  
-HeaderReader takeHeadInf(std::ifstream& input) { 
+uint32_t takeHeadInf(std::ifstream& input) {
     const auto sign = readArray<uint8_t, 2>(input);
-    HeaderReader h;
+    std::cout << "Сигнатура: " << sign.front() << sign.back() << std::endl;
 
-    h.sign1 = sign.front();
-    h.sign2= sign.back();
-
-    h.fileSize = takeNextField<uint32_t>(input);
+    takeNextField<uint32_t>(input);
 
     const auto reserved = readArray<uint16_t, 2>(input);
-    h.reserve1 = reserved.front();
-    h.reserve2 = reserved.back();
+    std::cout << "Зарезервированные поля (ожидается 0): " << reserved.front() << " " << reserved.back() << std::endl;
 
-    h.paddToData = takeNextField<uint32_t>(input);
-
-    return h;
+    return takeNextField<uint32_t>(input);
 }
 
-HeaderReader takeBitMapInfo(std::ifstream& input, HeaderReader h) { 
+Size takeBitMapInfo(std::ifstream& input) {
     uint32_t struck = takeNextField<uint32_t>(input);
 
-    if ((struck!=12) && (struck != 40) && (struck != 108) && (struck != 124)) // Проверка на валидность типа БМП
+    if ((struck != 12) && (struck != 40) && (struck != 108) && (struck != 124)) // Проверка на валидность типа БМП
         throw std::runtime_error("unknown BMP type");
 
-     switch (struck)
+    switch (struck)
     {
-        case 12:
-            throw std::runtime_error("cannot read this type of BMP (BITMAPCOREHEADER)");
-            break;
-        case 40:
-            std::cout << "Имя структуры: BITMAPINFOHEADER, файл корректный" << std::endl;
-            h.bmpType = struck;
-            break;
-        case 108:
-            throw std::runtime_error("cannot read this type of BMP (BITMAPV4HEADER)");
-            break;
-        case 124:
-            throw std::runtime_error("cannot read this type of BMP (BITMAPV5HEADER)");
-            break;
+    case 12:
+        throw std::runtime_error("cannot read this type of BMP (BITMAPCOREHEADER)");
+        break;
+    case 40:
+        std::cout << "Имя структуры: BITMAPINFOHEADER, файл корректный" << std::endl;
+        break;
+    case 108:
+        throw std::runtime_error("cannot read this type of BMP (BITMAPV4HEADER)");
+        break;
+    case 124:
+        throw std::runtime_error("cannot read this type of BMP (BITMAPV5HEADER)");
+        break;
     }
 
-     h.width = takeNextField<int32_t>(input);
-     h.height = takeNextField<int32_t>(input);
-     h.cursor= takeNextField<uint16_t>(input);
-     h.bpp = takeNextField<uint16_t>(input);
-     h.contain = takeNextField<uint32_t>(input);
-     h.pixDataSize = takeNextField<uint32_t>(input);
-     h.ppmX = takeNextField<int32_t>(input);
-     h.ppmY = takeNextField<int32_t>(input);
-     h.colorDataSize = takeNextField<uint32_t>(input);
-     h.cellsNum = takeNextField<uint32_t>(input);
-     
-    return h;
-} 
+    const auto width = takeNextField<int32_t>(input);
+    const auto height = takeNextField<int32_t>(input);
+    return { width, height };
+}
 
 char initializeargument(std::string arg)
 {
@@ -111,32 +95,20 @@ char initializeargument(std::string arg)
     return arg[1];
 }
 
-BMPdata filterCrop(Matrix mat, size_t newwidth, size_t newheight, HeaderReader h)
+Matrix filterCrop(Matrix mat, size_t newwidth, size_t newheight)
 {
-    h.width = newwidth;
-    h.height = newheight;
-    h.pixDataSize = newwidth * newheight * h.bpp;
-    h.fileSize = h.pixDataSize + h.paddToData;
-
-    BMPdata newpic;
-    newpic.setHeader(h);
     Matrix newmat(newwidth, newheight);
     for (size_t i = 0; i < newmat.GetMatHeight(); ++i) {
         for (size_t j = 0; j < newmat.GetMatWidth(); ++j) {
             newmat.SetPixel(i, j, mat.GetPixel(i, j));
         }
     }
-    newpic.setColorMatrix(newmat);
-   
-    return newpic;
+    return newmat;
 }
 
-BMPdata filterGs(Matrix mat, HeaderReader h)
+Matrix filterGs(Matrix mat)
 {
-    BMPdata newpic;
-    newpic.setHeader(h);
     Matrix newmat = mat;
-
     for (size_t i = 0; i < mat.GetMatHeight(); ++i) {
         for (size_t j = 0; j < mat.GetMatWidth(); ++j) {
             Pixel pix;
@@ -144,64 +116,57 @@ BMPdata filterGs(Matrix mat, HeaderReader h)
             newmat.SetPixel(i, j, pix);
         }
     }
-    newpic.setColorMatrix(newmat);
-   
-    return newpic;
+    return newmat;
 }
 
-BMPdata addFilters(std::vector<std::string> arg1, Matrix matr, HeaderReader h) 
+
+Matrix addFilters(std::vector<std::string> arg1, Matrix matr) 
 {
     std::vector<std::string> arg = arg1;
-    BMPdata newpic;
-    newpic.setHeader(h);
-    newpic.setColorMatrix(matr);
-    for (size_t i = 0; i<arg.size(); i++)
+    Matrix matrix = matr;
+    for (size_t i = 0; i < arg.size(); i++)
     {
-        if (arg[i][0]=='-') // InitializeToken
+        if (arg[i][0] == '-') // InitializeToken
         {
             char c = initializeargument(arg[i]);
             switch (c)
             {
-            case 'c': newpic = filterCrop(newpic.getMatrix(), stod(arg[i + 1]), stod(arg[i + 2]), newpic.getHeader()); i += 2; break;
-            case 'g': newpic = filterGs(newpic.getMatrix(), newpic.getHeader()); break;
+            case 'c': matrix = filterCrop(matrix, stod(arg[i + 1]), stod(arg[i + 2])); i += 2; break;
+            case 'g': matrix = filterGs(matrix); break;
             }
         }
     }
-   
-    return newpic;
+
+    return matrix;
 }
 
-BMPdata openAndFillImage(const std::string& filepath, std::vector<std::string> arg) // Считывание цветов в матрицу
+Matrix openAndFillImage(const std::string& filepath, std::vector<std::string> arg) // Считывание цветов в матрицу
 {
     std::ifstream input;
     std::string s1 = filepath;
     input.open(filepath, input.binary | input.in);
-   
+
     if (!input.is_open()) { // Проверка на открытие файла
-        /*Matrix nullmat(0, 0);
+        Matrix nullmat(0, 0);
         std::cout << "Файл не открыт";
-        return nullmat;*/
-        throw std::runtime_error("Файл не открыт, попробуйте снова");
+        return nullmat;
     }
 
-    HeaderReader head;
-
-    head = takeHeadInf(input);
-
-    const uint32_t pixelsOffset = head.paddToData;
-    head = takeBitMapInfo(input, head);
-    const size_t pixelsBytesPerLine = head.width * 3;
+    const uint32_t pixelsOffset = takeHeadInf(input);
+    const Size size = takeBitMapInfo(input);
+    const size_t pixelsBytesPerLine = size.width * 3;
     const size_t paddingBytes = pixelsBytesPerLine % 4;
 
     input.seekg(pixelsOffset);
 
     uint8_t r, g, b;
+
     Pixel pix;
 
-    Matrix mat(head.width, head.height);
+    Matrix mat(size.width, size.height);
 
-    for (size_t i = 0; i < head.height; ++i) {
-        for (size_t j = 0; j < head.width; ++j) {
+    for (size_t i = 0; i < size.height; ++i) {
+        for (size_t j = 0; j < size.width; ++j) {
             b = (unsigned)read<uint8_t>(input);
             g = (unsigned)read<uint8_t>(input);
             r = (unsigned)read<uint8_t>(input);
@@ -211,37 +176,69 @@ BMPdata openAndFillImage(const std::string& filepath, std::vector<std::string> a
         for (size_t pad = 0; pad != paddingBytes; ++pad) {
             read<uint8_t>(input);
         }
-    } 
-
-    return addFilters(arg, mat, head);
+    }
+    return addFilters(arg, mat);
 }
 
-void printMatrix(BMPdata pic, std::ostream& out) // Вывод матрицы
+HeaderReader makeHeader(Matrix mat)
 {
-    /*Matrix mat = matrix;
+    HeaderReader h;
+    h.width = mat.GetMatWidth();
+    h.height = mat.GetMatHeight();
+    h.pixDataSize = h.width * h.height * h.bpp;
+    h.fileSize = h.pixDataSize + h.paddToData;
+    return h;
+}
 
+void printMatrix(const Matrix& matrix, std::ostream& out) // Вывод матрицы
+{
+    Matrix mat = matrix;
+    HeaderReader bmpHead = makeHeader(mat);
+
+    std::cout << std::endl << bmpHead.sign1 << std::endl;
+    std::cout << bmpHead.sign2 << std::endl;
+    std::cout << bmpHead.fileSize << std::endl;
+    std::cout << bmpHead.reserve1 << std::endl;
+    std::cout << bmpHead.reserve2 << std::endl;
+    std::cout << bmpHead.paddToData << std::endl;
+    std::cout << bmpHead.bmpType << std::endl;
+    std::cout << bmpHead.width << std::endl;
+    std::cout << bmpHead.height << std::endl;
+    std::cout << bmpHead.cursor << std::endl;
+    std::cout << bmpHead.bpp << std::endl;
+    std::cout << bmpHead.contain << std::endl;
+    std::cout << bmpHead.pixDataSize << std::endl;
+    std::cout << bmpHead.ppmX << std::endl;
+    std::cout << bmpHead.ppmY << std::endl;
+    std::cout << bmpHead.colorDataSize << std::endl;
+    std::cout << bmpHead.cellsNum << std::endl;
+
+    out.write((char*)&bmpHead,sizeof bmpHead);
+    out.write((char*)&mat, sizeof mat);
+/*
     for (size_t i = 0; i < mat.GetMatHeight(); ++i) {
         for (size_t j = 0; j < mat.GetMatWidth(); ++j) {
-            mat.GetPixel(i, j).PrintPixel(out);
+            //mat.GetPixel(i, j).PrintPixel(out);
+       //     mat.GetPixel(i, j).PrintPixel();
         }
-        out << std::endl;
+        //out << std::endl;
+      //  std::cout << std::endl;
     }*/
-    pic.fillBMP(out);
 }
 
-int main (int files, char* data[])
+int main(int files, char* data[])
 {
     if (files < 3) throw std::runtime_error("input 2 paths next time");
     setlocale(LC_CTYPE, "Russian");
     std::vector<std::string> arg;
 
-    for (size_t i = 3; i<files; i++) // Создаём вектор аргмуентов
+    for (size_t i = 3; i < files; i++) // Создаём вектор аргмуентов
     {
         arg.push_back(data[i]);
     }
 
     std::ofstream output;
-    output.open(data[2], output.binary | output.out);
+    output.open(data[2], output.out);
 
     printMatrix(openAndFillImage(data[1], arg), output);
- }
+}
--- БМП/Photon2/Struct.h
+++ БМП/Photon2/Struct.h
@@ -10,21 +10,35 @@
 
 struct HeaderReader // Заполнение всех полей кроме размера файла и размера картинки автоматическое. Задача: рассчитать размер файла в байтах и пиксельных данных
 {
-    uint8_t sign1;
-    uint8_t sign2;
+    uint8_t sign1 = 'B';
+    uint8_t sign2 = 'M';
     uint32_t fileSize;
-    uint16_t reserve1;
-    uint16_t reserve2;
-    uint32_t paddToData;
-    uint32_t bmpType;
+    uint16_t reserve1 = 0;
+    uint16_t reserve2 = 0;
+    uint32_t paddToData = 54;
+    uint32_t bmpType = 40;
     size_t width;
     size_t height;
-    uint16_t cursor;
-    uint16_t bpp; // bit per pixel
-    uint32_t contain;
+    uint16_t cursor = 1;
+    uint16_t bpp = 24; // bit per pixel
+    uint32_t contain = 0;
     uint32_t pixDataSize;
-    uint32_t ppmX; // pixel per meter
-    uint32_t ppmY;
-    uint32_t colorDataSize;
-    uint32_t cellsNum; // Кол-во ячеек от начала таблицы цветов до последней используемой
+    uint32_t ppmX = 2835; // pixel per meter
+    uint32_t ppmY = 2835;
+    uint32_t colorDataSize = 0;
+    uint32_t cellsNum = 0; // Кол-во ячеек от начала таблицы цветов до последней используемой
+  /* HeaderReader()
+    {
+        sign1 = 'B';
+        sign2 = 'M';
+        reserve1 = reserve2 = 0;
+        paddToData = 54;
+        bmpType = 40;
+        cursor = 1;
+        ppmX = ppmY = 2835;
+        colorDataSize = 0;
+        cellsNum = 0;
+        contain = 0;
+        bpp = 24;
+    }*/
 };
